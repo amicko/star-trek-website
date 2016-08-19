d072122cd1bf9893822e15aa68f11a65638107d4
Aaron Micko <gmicko@hotmail.com>
2016-08-18 21:28:25 -0500

    Home Component structure complete, as well as base styling... excluding mobile version

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -33031,8 +33031,64 @@ module.exports = React.createClass({
 	render: function render() {
 		return React.createElement(
 			"div",
-			{ className: "titleContainer" },
-			"Also Rawr!"
+			{ className: "homeContainer" },
+			React.createElement(
+				"section",
+				{ id: "homeIntroduction" },
+				React.createElement(
+					"h2",
+					null,
+					"Introduction"
+				),
+				React.createElement(
+					"p",
+					null,
+					"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tellus turpis, vehicula eu placerat nec, venenatis sed massa. Ut ultrices ante ut lectus euismod porttitor. Duis ut condimentum est. Morbi non condimentum tortor, eget sagittis sem. Pellentesque pulvinar pellentesque justo sit amet faucibus. Vivamus rhoncus elit ut leo ultricies, a sodales nisl mattis. In maximus dictum orci, sit amet consectetur tellus ullamcorper tempor. Quisque a libero at est sagittis fringilla. Fusce pharetra ipsum in augue aliquam, at vehicula felis consectetur."
+				),
+				React.createElement(
+					"p",
+					null,
+					"Pellentesque erat massa, placerat pulvinar eros et, consectetur dapibus justo. Fusce tempor tellus id nibh auctor pharetra. Pellentesque venenatis a tortor non congue. Cras libero lectus, accumsan blandit euismod et, accumsan id velit. Vestibulum massa tellus, rhoncus eget mattis sit amet, elementum non felis. Duis ac accumsan diam. Nulla ut dignissim magna. Suspendisse vel consequat dolor, a varius purus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque pharetra libero id dignissim blandit. Fusce rutrum eros mauris, bibendum scelerisque velit molestie quis. In ut facilisis nisl."
+				)
+			),
+			React.createElement(
+				"section",
+				{ id: "homeFranchiseHistory" },
+				React.createElement(
+					"h2",
+					null,
+					"Franchise History"
+				),
+				React.createElement(
+					"p",
+					null,
+					"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tellus turpis, vehicula eu placerat nec, venenatis sed massa. Ut ultrices ante ut lectus euismod porttitor. Duis ut condimentum est. Morbi non condimentum tortor, eget sagittis sem. Pellentesque pulvinar pellentesque justo sit amet faucibus. Vivamus rhoncus elit ut leo ultricies, a sodales nisl mattis. In maximus dictum orci, sit amet consectetur tellus ullamcorper tempor. Quisque a libero at est sagittis fringilla. Fusce pharetra ipsum in augue aliquam, at vehicula felis consectetur."
+				),
+				React.createElement(
+					"p",
+					null,
+					"Pellentesque erat massa, placerat pulvinar eros et, consectetur dapibus justo. Fusce tempor tellus id nibh auctor pharetra. Pellentesque venenatis a tortor non congue. Cras libero lectus, accumsan blandit euismod et, accumsan id velit. Vestibulum massa tellus, rhoncus eget mattis sit amet, elementum non felis. Duis ac accumsan diam. Nulla ut dignissim magna. Suspendisse vel consequat dolor, a varius purus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque pharetra libero id dignissim blandit. Fusce rutrum eros mauris, bibendum scelerisque velit molestie quis. In ut facilisis nisl."
+				)
+			),
+			React.createElement(
+				"section",
+				{ id: "homePersonalHistory" },
+				React.createElement(
+					"h2",
+					null,
+					"Personal History"
+				),
+				React.createElement(
+					"p",
+					null,
+					"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tellus turpis, vehicula eu placerat nec, venenatis sed massa. Ut ultrices ante ut lectus euismod porttitor. Duis ut condimentum est. Morbi non condimentum tortor, eget sagittis sem. Pellentesque pulvinar pellentesque justo sit amet faucibus. Vivamus rhoncus elit ut leo ultricies, a sodales nisl mattis. In maximus dictum orci, sit amet consectetur tellus ullamcorper tempor. Quisque a libero at est sagittis fringilla. Fusce pharetra ipsum in augue aliquam, at vehicula felis consectetur."
+				),
+				React.createElement(
+					"p",
+					null,
+					"Pellentesque erat massa, placerat pulvinar eros et, consectetur dapibus justo. Fusce tempor tellus id nibh auctor pharetra. Pellentesque venenatis a tortor non congue. Cras libero lectus, accumsan blandit euismod et, accumsan id velit. Vestibulum massa tellus, rhoncus eget mattis sit amet, elementum non felis. Duis ac accumsan diam. Nulla ut dignissim magna. Suspendisse vel consequat dolor, a varius purus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque pharetra libero id dignissim blandit. Fusce rutrum eros mauris, bibendum scelerisque velit molestie quis. In ut facilisis nisl."
+				)
+			)
 		);
 	}
 });
--- scripts/components/HomeComponent.js
+++ scripts/components/HomeComponent.js
@@ -3,8 +3,34 @@ var React = require('react');
 module.exports = React.createClass({
 	render: function() {
 		return (
-			<div className="titleContainer">
-				Also Rawr!
+			<div className="homeContainer">
+				<section id="homeIntroduction">
+					<h2>Introduction</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tellus turpis, vehicula eu placerat nec, venenatis sed massa. Ut ultrices ante ut lectus euismod porttitor. Duis ut condimentum est. Morbi non condimentum tortor, eget sagittis sem. Pellentesque pulvinar pellentesque justo sit amet faucibus. Vivamus rhoncus elit ut leo ultricies, a sodales nisl mattis. In maximus dictum orci, sit amet consectetur tellus ullamcorper tempor. Quisque a libero at est sagittis fringilla. Fusce pharetra ipsum in augue aliquam, at vehicula felis consectetur.
+					</p>
+					<p>
+						Pellentesque erat massa, placerat pulvinar eros et, consectetur dapibus justo. Fusce tempor tellus id nibh auctor pharetra. Pellentesque venenatis a tortor non congue. Cras libero lectus, accumsan blandit euismod et, accumsan id velit. Vestibulum massa tellus, rhoncus eget mattis sit amet, elementum non felis. Duis ac accumsan diam. Nulla ut dignissim magna. Suspendisse vel consequat dolor, a varius purus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque pharetra libero id dignissim blandit. Fusce rutrum eros mauris, bibendum scelerisque velit molestie quis. In ut facilisis nisl.
+					</p>
+				</section>
+				<section id="homeFranchiseHistory">
+					<h2>Franchise History</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tellus turpis, vehicula eu placerat nec, venenatis sed massa. Ut ultrices ante ut lectus euismod porttitor. Duis ut condimentum est. Morbi non condimentum tortor, eget sagittis sem. Pellentesque pulvinar pellentesque justo sit amet faucibus. Vivamus rhoncus elit ut leo ultricies, a sodales nisl mattis. In maximus dictum orci, sit amet consectetur tellus ullamcorper tempor. Quisque a libero at est sagittis fringilla. Fusce pharetra ipsum in augue aliquam, at vehicula felis consectetur.
+					</p>
+					<p>
+						Pellentesque erat massa, placerat pulvinar eros et, consectetur dapibus justo. Fusce tempor tellus id nibh auctor pharetra. Pellentesque venenatis a tortor non congue. Cras libero lectus, accumsan blandit euismod et, accumsan id velit. Vestibulum massa tellus, rhoncus eget mattis sit amet, elementum non felis. Duis ac accumsan diam. Nulla ut dignissim magna. Suspendisse vel consequat dolor, a varius purus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque pharetra libero id dignissim blandit. Fusce rutrum eros mauris, bibendum scelerisque velit molestie quis. In ut facilisis nisl.
+					</p>
+				</section>
+				<section id="homePersonalHistory">
+					<h2>Personal History</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tellus turpis, vehicula eu placerat nec, venenatis sed massa. Ut ultrices ante ut lectus euismod porttitor. Duis ut condimentum est. Morbi non condimentum tortor, eget sagittis sem. Pellentesque pulvinar pellentesque justo sit amet faucibus. Vivamus rhoncus elit ut leo ultricies, a sodales nisl mattis. In maximus dictum orci, sit amet consectetur tellus ullamcorper tempor. Quisque a libero at est sagittis fringilla. Fusce pharetra ipsum in augue aliquam, at vehicula felis consectetur.
+					</p>
+					<p>
+						Pellentesque erat massa, placerat pulvinar eros et, consectetur dapibus justo. Fusce tempor tellus id nibh auctor pharetra. Pellentesque venenatis a tortor non congue. Cras libero lectus, accumsan blandit euismod et, accumsan id velit. Vestibulum massa tellus, rhoncus eget mattis sit amet, elementum non felis. Duis ac accumsan diam. Nulla ut dignissim magna. Suspendisse vel consequat dolor, a varius purus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque pharetra libero id dignissim blandit. Fusce rutrum eros mauris, bibendum scelerisque velit molestie quis. In ut facilisis nisl.
+					</p>
+				</section>
 			</div>
 		)
 	}
